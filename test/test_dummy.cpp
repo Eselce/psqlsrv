378db9996895a2fde59fc08c0a249aafbd4df676
SLC <S.Loges@gmx.de>
2026-02-13 14:21:59 +0100

    test_dummy.?pp: Added dummy unit test for catch2

---
+++ test/test_dummy.cpp
@@ -0,0 +1,7 @@
+
+#include "test_dummy.hpp"
+
+TEST_CASE("sum works", "[sum]") {
+    REQUIRE(1 + 2 == 3);
+    REQUIRE(-1 + 1 == 0);
+}
---
+++ test/test_dummy.hpp
@@ -0,0 +1,8 @@
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+
+#include "pgconn.hpp"
+#include "httpsrv.hpp"
+
