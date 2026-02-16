8f62cdf2b798c168833dbbbc566fe646e24bce8f
SLC <S.Loges@gmx.de>
2026-02-16 22:04:56 +0100

    Optional start HTTP/DB/both/none, some test queries

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 
+#define _START_HTTP_SERVER_MAIN
+#define _START_PG_CONNECTION_MAIN
+
 #include "main.hpp"
 
 int main(int argc, char **argv)
 {
+#if defined(_START_HTTP_SERVER_MAIN)
+	bool           ok       = false;
+#else
+	bool           ok       = true;
+#endif
+
+#if defined(_START_PG_CONNECTION_MAIN)
+	std::cout << "STARTING POSTGRESQL CONNECTION" << std::endl;
+
 	const char    *conninfo;
 	PGconnection   pg;
-	bool           ok       = false;
 
 	if (argc > 1) {
 		conninfo = argv[1];
@@ -15,17 +26,49 @@ int main(int argc, char **argv)
 
 	pg.connect(conninfo, true);
 
-	if (pg.check()) {
+	if (pg.checkconnect()) {
 		std::cout << "CONNECTED" << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SELECT version();") << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SELECT current_database();") << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SELECT current_user;") << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SELECT current_schema();") << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SET search_path TO loges;") << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SELECT 1;") << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SELECT * FROM pg_type LIMIT 10;") << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SELECT * FROM orders LIMIT 3;") << std::endl;
+
+		std::cout << "ANSWER: " << pg.getanswer("SELECT zahl, klein FROM test2 WHERE \"ID\" = 2;") << std::endl;
+#endif
+
+#if defined(_START_HTTP_SERVER_MAIN)
+		std::cout << "STARTING HTTP SERVER" << std::endl;
 
 		HTTPserver srv;
 
 		srv.setupStatic();
 
-		ok = srv.start();
+#if defined(_START_PG_CONNECTION_MAIN)
+		srv.setDBConnection(&pg);
+#endif
 
+		ok = srv.start();
+#endif
+
+#if defined(_START_PG_CONNECTION_MAIN)
 		pg.disconnect();
+	} else {
+		std::cerr << "CONNECTION FAILED" << std::endl;
+		ok = false;
 	}
+#endif
 
 	return (ok ? 0 : -1);
 }
