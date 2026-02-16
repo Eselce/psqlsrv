411edb7f15a26e4d2c9eab44acfb8ad067111b2e
SLC <S.Loges@gmx.de>
2026-02-16 22:03:18 +0100

    getanswer(): String output for whole table (matrix)

--- src/pganswer.cpp
+++ src/pganswer.cpp
@@ -1,4 +1,6 @@
 
+#include <string>
+
 #include "pganswer.hpp"
 
 PGanswer::PGanswer(PGresult *res)
@@ -9,5 +11,37 @@ PGanswer::PGanswer(PGresult *res)
 PGanswer::~PGanswer(void)
 {
 	PQclear(m_pRes);
+}
+
+const std::string PGanswer::getanswer(void) const
+{
+	ExecStatusType status = PQresultStatus(m_pRes);
+
+	if (status == PGRES_TUPLES_OK) {
+		int ntuples = PQntuples(m_pRes);
+		int nfields = PQnfields(m_pRes);
+		std::string answer = "";
+
+		for (int row = 0; row < ntuples; ++row) {
+			for (int col = 0; col < nfields; ++col) {
+				char *value = PQgetvalue(m_pRes, row, col);
+
+				answer += (value ? value : "NULL");
+
+				if (col < (nfields - 1)) {
+					answer += ",";
+				}
+			}
+
+			if (row < (ntuples - 1)) {
+				answer += "\n";
+			}
+		}
+		return answer;
+	} else if (status == PGRES_COMMAND_OK) {
+		return "OK";
+	} else {
+		return "";
+	}
 }
 
