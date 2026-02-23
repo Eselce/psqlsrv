d1ac6de178fe66f68252d8cecbaabbee73fe054e
SLC <S.Loges@gmx.de>
2026-02-23 14:16:22 +0100

    With pg_type.h (changed) now being global PG header, added db_type.h for DB realm

---
+++ include/db_type.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+// General C++ type for type parameter in DBparameter::bindany()...
+typedef unsigned int DBparameterType;
+
+// Possible values for format parameter in DBparameter::bindany()...
+typedef enum
+{
+	FORMAT_TEXT	= 0,	// 0: FORMAT_TEXT   - Text format
+	FORMAT_BINARY		// 1: FORMAT_BINARY - Binary format
+} DBparameterFormat;
+
+class DBconnection;  // forward declaration to avoid circular dependency
+
+extern const char *cmdErrorMsg;  // = "Command failed";
+
--- include/pg_type.h
+++ include/pg_type.h
@@ -1,6 +1,12 @@
 
 #pragma once
 
+#include "postgresql/libpq-fe.h"
+
+#include "postgresql/pg_config.h"
+
+class PGconnection;  // forward declaration to avoid circular dependency
+
 // Substitution for server side pg_type.h
 // until it's clear what the fuzz is about!
 
@@ -54,6 +60,126 @@
 #define NUMERICOID	    1700
 #define JSONBOID	    3802
 #define UUIDOID		    2950
+
+#endif
+
+#if defined(_DEBUG)
+
+// Names of some constants (primitive implementation)
+// TODO Create a more clever implementation. Debug only.
+
+/* Application-visible enum types */
+
+/*
+ * Although it is okay to add to these lists, values which become unused
+ * should never be removed, nor should constants be redefined - that would
+ * break compatibility with existing code.
+ */
+
+[[maybe_unused]] static const char *ConnStatusTypeName[] =
+{
+	"CONNECTION_OK",
+	"CONNECTION_BAD",
+	/* Non-blocking mode only below here */
+
+	/*
+	 * The existence of these should never be relied upon - they should only
+	 * be used for user feedback or similar purposes.
+	 */
+	"CONNECTION_STARTED",		/* Waiting for connection to be made.  */
+	"CONNECTION_MADE",			/* Connection OK; waiting to send.     */
+	"CONNECTION_AWAITING_RESPONSE",	/* Waiting for a response from the
+									 * postmaster.        */
+	"CONNECTION_AUTH_OK",		/* Received authentication; waiting for
+								 * backend startup. */
+	"CONNECTION_SETENV",		/* This state is no longer used. */
+	"CONNECTION_SSL_STARTUP",	/* Negotiating SSL. */
+	"CONNECTION_NEEDED",		/* Internal state: connect() needed */
+	"CONNECTION_CHECK_WRITABLE",/* Checking if session is read-write. */
+	"CONNECTION_CONSUME",		/* Consuming any extra messages. */
+	"CONNECTION_GSS_STARTUP",	/* Negotiating GSSAPI. */
+	"CONNECTION_CHECK_TARGET",	/* Checking target server properties. */
+	"CONNECTION_CHECK_STANDBY"	/* Checking if server is in standby mode. */
+};
+
+[[maybe_unused]] static const char *PostgresPollingStatusTypeName[] =
+{
+	"PGRES_POLLING_FAILED",
+	"PGRES_POLLING_READING",	/* These two indicate that one may	  */
+	"PGRES_POLLING_WRITING",	/* use select before polling again.   */
+	"PGRES_POLLING_OK",
+	"PGRES_POLLING_ACTIVE"		/* unused; keep for awhile for backwards
+								 * compatibility */
+};
+
+[[maybe_unused]] static const char *ExecStatusTypeName[] =
+{
+	"PGRES_EMPTY_QUERY",		/* empty query string was executed */
+	"PGRES_COMMAND_OK",			/* a query command that doesn't return
+								 * anything was executed properly by the
+								 * backend */
+	"PGRES_TUPLES_OK",			/* a query command that returns tuples was
+								 * executed properly by the backend, PGresult
+								 * contains the result tuples */
+	"PGRES_COPY_OUT",			/* Copy Out data transfer in progress */
+	"PGRES_COPY_IN",			/* Copy In data transfer in progress */
+	"PGRES_BAD_RESPONSE",		/* an unexpected response was recv'd from the
+								 * backend */
+	"PGRES_NONFATAL_ERROR",		/* notice or warning message */
+	"PGRES_FATAL_ERROR",		/* query failed */
+	"PGRES_COPY_BOTH",			/* Copy In/Out data transfer in progress */
+	"PGRES_SINGLE_TUPLE",		/* single tuple from larger resultset */
+	"PGRES_PIPELINE_SYNC",		/* pipeline synchronization point */
+	"PGRES_PIPELINE_ABORTED"	/* Command didn't run because of an abort
+								 * earlier in a pipeline */
+};
+
+[[maybe_unused]] static const char *PGTransactionStatusTypeName[] =
+{
+	"PQTRANS_IDLE",				/* connection idle */
+	"PQTRANS_ACTIVE",			/* command in progress */
+	"PQTRANS_INTRANS",			/* idle, within transaction block */
+	"PQTRANS_INERROR",			/* idle, within failed transaction */
+	"PQTRANS_UNKNOWN"				/* cannot determine status */
+};
+
+[[maybe_unused]] static const char *PGVerbosityName[] =
+{
+	"PQERRORS_TERSE",			/* single-line error messages */
+	"PQERRORS_DEFAULT",			/* recommended style */
+	"PQERRORS_VERBOSE",			/* all the facts, ma'am */
+	"PQERRORS_SQLSTATE"			/* only error severity and SQLSTATE code */
+};
+
+[[maybe_unused]] static const char *PGContextVisibilityName[] =
+{
+	"PQSHOW_CONTEXT_NEVER",		/* never show CONTEXT field */
+	"PQSHOW_CONTEXT_ERRORS",	/* show CONTEXT for errors only (default) */
+	"PQSHOW_CONTEXT_ALWAYS"		/* always show CONTEXT field */
+};
+
+/*
+ * PGPing - The ordering of this enum should not be altered because the
+ * values are exposed externally via pg_isready.
+ */
+
+[[maybe_unused]] static const char *PGPingName[] =
+{
+	"PQPING_OK",				/* server is accepting connections */
+	"PQPING_REJECT",			/* server is alive but rejecting connections */
+	"PQPING_NO_RESPONSE",		/* could not establish connection */
+	"PQPING_NO_ATTEMPT"			/* connection not attempted (bad params) */
+};
+
+/*
+ * PGpipelineStatus - Current status of pipeline mode
+ */
+[[maybe_unused]] static const char *PGpipelineStatusName[] =
+{
+	"PQ_PIPELINE_OFF",
+	"PQ_PIPELINE_ON",
+	"PQ_PIPELINE_ABORTED"
+};
 
 #endif
 
