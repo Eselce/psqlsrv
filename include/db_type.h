a5b66f7031cb5e9322b69f206c9a3461af46a36f
SLC <S.Loges@gmx.de>
2026-03-01 00:03:48 +0100

    db_type.h: Aligned comments

--- include/db_type.h
+++ include/db_type.h
@@ -7,13 +7,13 @@ typedef unsigned int DBparameterType;
 // Possible values for format parameter in DBparameter::bindany()...
 typedef enum
 {
-	FORMAT_TEXT	= 0,	// 0: FORMAT_TEXT   - Text format
-	FORMAT_BINARY		// 1: FORMAT_BINARY - Binary format
+	FORMAT_TEXT	= 0,			// 0: FORMAT_TEXT   - Text format
+	FORMAT_BINARY				// 1: FORMAT_BINARY - Binary format
 } DBparameterFormat;
 
-class DBconnection;  // forward declaration to avoid circular dependency
+class DBconnection;				// forward declaration to avoid circular dependency
 
-extern const char *cmdErrorMsg;  // = "Command failed";
+extern const char *cmdErrorMsg;	// = "Command failed";
 
-extern const char *cmdLogMsg;  // = "Execute command (status)";
+extern const char *cmdLogMsg;	// = "Execute command (status)";
 
